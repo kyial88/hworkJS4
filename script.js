dea608e113d8896a1e10cc05fcd05ae5ee07120a
Kind <138190978+kyial88@users.noreply.github.com>
2023-07-13 20:36:11 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,223 @@
+
+ // Home Work
+// user admin true welcome, 'user'
+// false not correct user function checkUser(user)
+
+// function checkUser(user){
+//     if (user === 'admin') {
+//         return 'welcome'
+//     } else {
+//         return 'not correct user'
+//     }
+// }
+// console.log(checkUser('admin'))
+
+
+
+// today is 'day' of february
+// on february only 28 day, your day is 'day' of another month
+
+// function checkUser(day) {
+//     if (day >= 1 && day <= 28){
+//     return `today is $(day) of february`}
+//     else{
+//     return  `on february only 28 day, your day is ${day} of another month` }
+// }
+//      console.log(checkUser(-1))
+
+
+// 'num'// 'num' is equals of 50
+// 'num' is not correct
+
+// function numFunction(num) {
+//     if (num === 50){
+//         return `$(num) is equals of 50 `
+//     }else {
+//         return `$(num) is not correct `
+//     }
+// }
+//
+// console.log(numFunction(100))
+
+
+// 'user' 'age' old, welcome - выполнить проверку на возраст и вывести имя и возраст. Должно быть +18
+// 'user' 'age' old, rejected - если проверка не пройдена, то вывести это сообщение
+
+
+// function checkUser(user, age){
+//     if (user >='kyial'){
+//         return `${age} old, welcome`
+//     }else { return `${user} old, rejected`}
+//     if (age >=18){
+//         return `${user} old,welcome`
+//     }else { return `${age} old,welcome`}
+// }
+//
+//     console.log(checkUser('kyial',18))
+
+
+// 'user' name is too short (min 3)
+// 'user' name is accepted
+
+// 'user' name is too long (max 5)
+// 'user' name is accepted
+
+// function handleCheck(str){
+//     if(str.length <=5 && str.length >=3){
+//         return true;
+//     }else {
+//         return false;
+//     }
+// }
+// console.log(handleCheck("2"))
+
+
+// 'age' must be a number ('20')
+
+// function checkUser(age){
+//     if (typeof 'age === 20'){
+//         return 'must be a number'
+//     }else {return 'is not correct'}
+// }
+//
+// console.log(checkUser('age === 20' ))
+
+// age' must be a string (20)
+// 'age' must be older then 23
+
+// function handleCheck(age){
+//     if (typeof age === '20'){
+//         return 'must be a string'
+//     }else {return 'must be older then 23'}
+// }
+//
+// console.log(handleCheck('20'))
+
+
+
+// today is monday/tuesday/wednesday/thursday/friday weekend
+// max days of weeks 7
+
+// function handleDayOfWeek(day){
+//     switch (day) {
+//         case 1:
+//             return "Monday";
+//         case 2:
+//             return "Tuesday";
+//         case 3:
+//             return "Wednesday";
+//         case 4:
+//             return "Thursday";
+//         case 5:
+//             return "Friday";
+//         case 6:
+//             return "Saturday";
+//         case 7:
+//             return "Weekend";
+//         default:
+//             return "max days of weeks 7"
+//     }
+// }
+//
+// console.log(handleDayOfWeek(7))
+
+// hello world
+// 1. вам надо будет проверить и убрать пробелы в начале и в конце
+// 2. поменять местами world hello
+// 3. не первое o поменять на а
+
+// 1. проверить длинну на четность
+
+// 1. lorem ipsum dolor sit amet => LoremIpsumDolorSitAmet
+// price и discount
+
+// str в задаче вам надо проверить что это строка.
+// вы проверяете не пустая ли она. длина мин 4 макс 10. четность длины.
+
+// function handleCheck(str){
+//     str = str.trim()
+//     str = str.slice(str.indexOf(' ')+1)+' '+str.slice(0,str.indexOf(' '))
+//     return str
+//     str = 'hello world'
+//     str = str.replace(/hello world/g,"o" ==>"a")
+// }
+// console.log(handleCheck('hello world'))
+
+
+// function isEven(string){
+//
+//     if(string.length % 2 === 0){
+//         return 'jhfjhgjhgjhg'
+//     } else {
+//         return '564654654654'
+}
+// console.log(isEven(string(1)))
+// lorem ipsum dolor sit amet
+// function handleCheck(text){
+//     if (text) {
+//         return text.replaceAll(' ', '')
+//     }else {
+//         return undefined}
+// }
+//
+// console.log(handleCheck(' lorem ipsum dolor sit amet'))
+
+// function handleCheck(price,discount){
+//     if price( price:'10', discount:'0.15'){
+//         return (this.price - (this.discount / this.price))
+//     }
+//
+// }
+//
+// console.log(handleCheck(1000,5))
+
+
+// str в задаче вам надо проверить что это строка.
+// вы проверяете не пустая ли она. длина мин 4 макс 10. четность длины.
+
+
+// function isEmpty(str) {
+//     if (str.length! === 0){
+//         return true
+//     }else {
+//        return false
+//     }if (str.trim() === ''){
+//        return true
+//     }else {
+//         return false}
+//
+// }
+// console.log(isEmpty(5))
+//
+
+// function printEntries(line){
+//     if (line.length >=4 && line.length < 11 )
+//         if (line.length % 2 === 0){
+//             return 'false'
+//         }else{
+//             return 'true'
+//         }
+//
+// }
+// console.log(printEntries(0))
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
